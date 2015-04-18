c2108e1d219f036cd9304c63ea66ed83a6e73c9c
nayzo <alakhefifi@gmail.com>
2015-04-18 19:09:39 +0200

    Add services.xml

--- DependencyInjection/NzoGrabberExtension.php
+++ DependencyInjection/NzoGrabberExtension.php
@@ -15,6 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * Class NzoGrabberExtension
@@ -30,7 +31,7 @@ class NzoGrabberExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
